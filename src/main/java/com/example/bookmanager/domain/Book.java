40ad9bc0cd46cbf6bd45f63f466eec6db1f302db
yurison <yu_ri_son@naver.com>
2023-08-07 09:43:02 +0900

    BaseEntity 도입

--- src/main/java/com/example/bookmanager/domain/Book.java
+++ src/main/java/com/example/bookmanager/domain/Book.java
@@ -1,22 +1,15 @@
 package com.example.bookmanager.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
-@EntityListeners(value = AuditingEntityListener.class)
-public class Book {
+public class Book extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -25,11 +18,5 @@ public class Book {
     private String name;
 
     private String author;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 
 }
--- src/main/java/com/example/bookmanager/domain/Member.java
+++ src/main/java/com/example/bookmanager/domain/Member.java
@@ -1,20 +1,16 @@
 package com.example.bookmanager.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
-@EntityListeners(value = AuditingEntityListener.class)
-public class Member{
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -25,12 +21,5 @@ public class Member{
 
     @NonNull
     private String email;
-
-    @Column(updatable = false)
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 
 }
