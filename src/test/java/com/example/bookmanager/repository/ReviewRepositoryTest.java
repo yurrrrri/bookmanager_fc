47c89da1d7db30dcfd2b02185c2dc9f374289fc9
yurison <yu_ri_son@naver.com>
2023-08-15 22:25:40 +0900

    ReviewRepository 2

--- src/test/java/com/example/bookmanager/repository/ReviewRepositoryTest.java
+++ src/test/java/com/example/bookmanager/repository/ReviewRepositoryTest.java
@@ -4,16 +4,18 @@ import com.example.bookmanager.domain.Review;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @SpringBootTest
-public class ReviewRepositoryTest {
+class ReviewRepositoryTest {
 
     @Autowired
     private ReviewRepository reviewRepository;
 
     @Test
+    @Transactional
     void reviewTest() {
         List<Review> reviews = reviewRepository.findAll();
     }
